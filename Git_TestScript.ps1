6ab86bd0a3d2b4f781e5aaa3d675859455bef73e
SMSivaneson <bshivha@gmail.com>
2019-09-18 16:40:31 +0530

    My V1.0 update

--- Git_TestScript.ps1
+++ Git_TestScript.ps1
@@ -1,4 +1,11 @@
 ﻿# MY First Script to test GIT Repository
 
 #List Running Services which are starting with 'A'
-Get-Service A* | ? {$_.Status -eq 'Running'}
+$svc = Get-Service A* | ? {$_.Status -eq 'Running'}
+
+foreach ($service in $svc) {
+
+   Write-Host "$($service.name),$($service.status)"
+
+}
+
